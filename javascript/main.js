c01c8d32fd3acbdba5a235a3ba511e84c5589e3a
SymBiios <julien.sarkarin@gmail.com>
2025-05-22 15:05:36 +0200

    feat: add basics function

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,67 @@
 "use strict";
+
+const startSection = document.getElementById("startSection");
+const startBtn = document.getElementById("startBtn");
+const battleSection = document.getElementById("battleSection");
+
+const monsterHealthText = document.getElementById("monsterHealthText");
+const playerHealthText = document.getElementById("playerHealthText");
+const attackBtn = document.getElementById("attackBtn");
+const specialBtn = document.getElementById("specialBtn");
+const healBtn = document.getElementById("healBtn");
+const abandonBtn = document.getElementById("abandonBtn");
+
+specialBtn.disabled = true;
+let attackCount = 0;
+
+startBtn.addEventListener("click", function (event) {
+  startSection.classList.add("hidden");
+  startBtn.classList.add("hidden");
+  battleSection.classList.remove("hidden");
+});
+
+attackBtn.addEventListener("click", function (event) {
+  if (attackCount < 2) {
+    attackCount++;
+  }
+  let humanAttack = Math.floor(Math.random() * 10) + 2;
+  let monsterHealth = monsterHealthText.textContent;
+  monsterHealthText.textContent = monsterHealth - humanAttack;
+  monsterAlive();
+  specialEnable(attackCount);
+});
+
+function specialEnable(attackCount) {
+  if (attackCount == 2) {
+    specialBtn.disabled = false;
+  }
+}
+
+specialBtn.addEventListener("click", function (event) {
+  let specialAttack = Math.floor(Math.random() * 20) + 15;
+  let monsterHealth = monsterHealthText.textContent;
+  monsterHealthText.textContent = monsterHealth - specialAttack;
+  specialBtn.disabled = true;
+  monsterAlive();
+});
+
+healBtn.addEventListener("click", function (event) {
+  let playerHealth = playerHealthText.value;
+  playerHealth += 50;
+  playerHealthText.value = playerHealth;
+});
+
+abandonBtn.addEventListener("click", function (event) {});
+
+function monsterAttack() {
+  let monsterAttack = Math.floor(Math.random() * 10) + 10;
+  let playerHealth = playerHealthText.textContent;
+  playerHealthText.textContent = playerHealth - monsterAttack;
+}
+
+function monsterAlive() {
+  if (monsterHealthText.content <= 0) {
+  } else {
+    monsterAttack();
+  }
+}
